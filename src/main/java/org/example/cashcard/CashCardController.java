f548ef5c6215a50d07339591f7925f851551826b
EvVlF <ev.vl.fedorov@gmail.com>
2024-02-01 21:56:37 +0400

    configure Controller to listen for and handle HTTP requests to /cashcards

--- src/main/java/org/example/cashcard/CashCardController.java
+++ src/main/java/org/example/cashcard/CashCardController.java
@@ -1,8 +1,26 @@
 package org.example.cashcard;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/*
+ @RestController
+This tells Spring that this class is a Component of type RestController and capable of handling HTTP requests.
+
+ @RequestMapping("/cashcards")
+This is a companion to @RestController that indicates which address requests must have to access this Controller.
+ */
+@RestController
+@RequestMapping("/cashcards")
 class CashCardController {
+
+    /*
+    @GetMapping marks a method as a handler method.
+    GET requests that match cashcards/{requestedID} will be handled by this method.
+     */
+    @GetMapping("/{requestedId}")
     private ResponseEntity<String> findById() {
         return ResponseEntity.ok("{}");
     }
