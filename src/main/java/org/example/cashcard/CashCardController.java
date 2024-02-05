2ff8a281075edc4e574e0e203b6ad4b9056afe77
EvVlF <ev.vl.fedorov@gmail.com>
2024-02-05 20:11:37 +0400

    update Controller for return correct id

--- src/main/java/org/example/cashcard/CashCardController.java
+++ src/main/java/org/example/cashcard/CashCardController.java
@@ -22,7 +22,7 @@ class CashCardController {
      */
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById() {
-        CashCard cashCard = new CashCard(1000L, 0.0);
+        CashCard cashCard = new CashCard(99L, 0.0);
         return ResponseEntity.ok(cashCard);
     }
 }
