c207cc445c551c1f7a4f7888a905997def15664f
EvVlF <ev.vl.fedorov@gmail.com>
2024-02-06 20:53:10 +0400

    parametrize CrudRepository (CashCardRepository) with <CashCard, Long>

--- src/main/java/org/example/cashcard/CashCardRepository.java
+++ src/main/java/org/example/cashcard/CashCardRepository.java
@@ -8,5 +8,5 @@ When we extend it (or other sub-Interfaces of Spring Data's Repository),
 Spring Boot and Spring Data work together to automatically generate the CRUD methods
 that we need to interact with a database.
  */
-public interface CashCardRepository extends CrudRepository {
+public interface CashCardRepository extends CrudRepository<CashCard, Long> {
 }
