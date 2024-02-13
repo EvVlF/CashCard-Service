bb385d771624990fe540ef366c6fde70689ceae4
EvVlF <ev.vl.fedorov@gmail.com>
2024-02-13 20:16:58 +0400

    add Sort.by(Sort.Direction.ASC, "amount") in Controller

--- src/main/java/org/example/cashcard/CashCardController.java
+++ src/main/java/org/example/cashcard/CashCardController.java
@@ -90,7 +90,7 @@ class CashCardController {
                 PageRequest.of(
                         pageable.getPageNumber(),
                         pageable.getPageSize(),
-                        pageable.getSort()
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
                 ));
         return ResponseEntity.ok(page.getContent());
     }
