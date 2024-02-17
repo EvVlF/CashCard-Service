909b53b50d94cfd215cf4e4355907a339cd7d71e
EvVlF <ev.vl.fedorov@gmail.com>
2024-02-17 12:29:38 +0400

    enable Spring Security

--- src/main/java/org/example/cashcard/SecurityConfig.java
+++ src/main/java/org/example/cashcard/SecurityConfig.java
@@ -1,13 +1,25 @@
 package org.example.cashcard;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+/*
+@Configuration annotation tells Spring to use this class to configure Spring and Spring Boot itself.
+Any Beans specified in this class will now be available to Spring's Auto Configuration engine.
+ */
+@Configuration
 public class SecurityConfig {
 
+    /*
+    Spring Security expects a Bean to configure its Filter Chain,
+    which you learned about in the Simple Spring Security lesson.
+    Annotating a method returning a SecurityFilterChain with the @Bean satisfies this expectation.
+     */
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.build();
     }
