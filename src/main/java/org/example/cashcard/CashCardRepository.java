e7d4c07f10cae1f1c13c67ce4a54a7cdacb0bca0
EvVlF <ev.vl.fedorov@gmail.com>
2024-02-17 15:05:26 +0400

    update the CashCardRepository with new findBy... methods for filter data access by CashCard owner

--- src/main/java/org/example/cashcard/CashCardRepository.java
+++ src/main/java/org/example/cashcard/CashCardRepository.java
@@ -3,11 +3,16 @@ package org.example.cashcard;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
 /*
 CrudRepository is an interface supplied by Spring Data.
 When we extend it (or other sub-Interfaces of Spring Data's Repository),
 Spring Boot and Spring Data work together to automatically generate the CRUD methods
 that we need to interact with a database.
  */
-public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+    CashCard findByIdAndOwner(Long id, String owner);
+    Page<CashCard> findByOwner(String owner, PageRequest pageRequest);
 }
