baaff12168d227ab4d625cb379fa96a6f814fc6b
EvVlF <ev.vl.fedorov@gmail.com>
2024-02-21 20:44:34 +0400

    update CashCardController.findById

--- src/main/java/org/example/cashcard/CashCardController.java
+++ src/main/java/org/example/cashcard/CashCardController.java
@@ -42,7 +42,7 @@ class CashCardController {
 
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId, Principal principal) {
-        CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        CashCard cashCard = findCashCard(requestedId, principal);
         if (cashCard != null) {
             return ResponseEntity.ok(cashCard);
         } else {
